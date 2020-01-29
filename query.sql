c90b4b5232be0c4d8cad86a95279abf97e2a399e
Nothile Ngcobo <nothilengcobo@nothiles-MacBook-Pro.local>
2020-01-29 10:13:27 +0200

    refactored sql query

--- query.sql
+++ query.sql
@@ -1,64 +1,44 @@
-
--- #1 select all records from the customer 
-SELECT * FROM Customer;
+SELECT * FROM Customers;
 ​
--- #2 select name column from customer 
-SELECT First_Name FROM Customer;
+SELECT firstname FROM Customers;
 ​
--- #3 dispaly the result for ID=1
-SELECT First_Name FROM Customer WHERE Customer_ID=1;
+SELECT firstname FROM Customers WHERE Customerid=1;
 
--- #4 update record from id=1
-​UPDATE Customer SET First_Name='Nothile Ngcobo' WHERE Customer_ID=1;
+​UPDATE customers SET firstname = 'Nothile Ngcobo' WHERE customerid=1;
 
--- #5 delete record for id=1 
-​DELETE FROM Customer WHERE Customer_ID=1;
+​DELETE FROM Customers WHERE Customerid=1;
 ​
--- #6 display all unique status from orders
 SELECT COUNT(Status) FROM Orders;
 ​
--- #7 dispaly the max payment from payments
 SELECT MAX(Amount) FROM Payments;
 ​
- -- #8 arange data from customer by country
-SELECT * FROM Customer ORDER BY Country ASC;
+SELECT * FROM Customers ORDER BY Country ASC;
 ​
--- #9  select all products with price 
 SELECT * FROM products WHERE price BETWEEN '100' AND '600';
 
--- #10 Select * from “Customers” where country is “Germany” AND city is “Berlin”.
-SELECT * FROM customer
+
+SELECT * FROM customers
 WHERE city='Berlin'
 AND country='Germany';
 
--- #11 Select  * from “Customers” where city is “Cape Town” OR “Durban”.
-SELECT * FROM customer
+SELECT * FROM Customers
 WHERE city='Cape Town'
 or city='durban';
 
--- #12 Select * records from Products where the Price is GREATER than R500.
 SELECT * FROM products
-WHERE price > '500';
-
--- #13 Return the sum of the Amounts on the Payments table.
+WHERE buyprice > '500';
 
 SELECT SUM(amount) FROM payments;
 
--- #14 Count the number of shipped orders in the Orders table.
 SELECT count(*) FROM orders
-WHERE status='Shipped';
+WHERE parcelStatus='Shipped';
 
--- #15 Return the avg price of * Products, in R 
+SELECT AVG(price) FROM products;
 
-
-SELECT AVG(price) FROM products
-
--- #16 Using INNER JOIN create a query that selects * Payments with Customer information.
-SELECT customer.customer_ID, payments.payment_ID
-FROM customer
+SELECT customers.customerId, payments.paymentid
+FROM customers
 INNER JOIN payments
-ON customer.customer_ID = payments.customer_ID
+ON customers.customerid = payments.customerid;
 
---  #17 Select all products that have turnable front wheels.
 SELECT * FROM products 
 WHERE descriptions LIKE 'Turnable front wheels%';
--- tables.sql
+++ tables.sql
@@ -1,63 +1,80 @@
--- 1 create database called umuzi
-CREATE  DATABASE umuzi;
 
--- #2 create the tables 
+DROP TABLE IF EXISTS Customers, products, employees, payments, orders;
 
 
-
-CREATE TABLE Customer(
-Customer_ID     INT,
-First_Name      VARCHAR(50),
-Last_Name       VARCHAR(50),
-Gender          VARCHAR,
-Address         VARCHAR(200),
-Phone           INTEGER,
-Email           VARCHAR(100),
-City            VARCHAR(20),
-Country         VARCHAR(50)
+CREATE TABLE Customers(
+customerid     SERIAL PRIMARY KEY,
+firstname      VARCHAR (50),
+lastname       VARCHAR(50),
+gender          VARCHAR,
+addresses       VARCHAR(200),
+phone           bigint,
+email           VARCHAR(100),
+city            VARCHAR(20),
+country         VARCHAR(50)
 );
 
 
 CREATE TABLE employees(
-employee_id     INT,
-First_Name      VARCHAR(50),
-Last_Name       VARCHAR(50),
-Email         VARCHAR,
-job_title
+employeeid     SERIAL primary key,
+firstname      VARCHAR(50),
+lastName       VARCHAR(50),
+email         VARCHAR,
+jobTitle       varchar
 );
 
 
-CREATE TABLE orders(
-order_id     INT,
-product_id      INT,
-payment_id       INT,
-employee_id      INT,
-daterequires     date(200),
-dateshipped      INTEGER,
-status           VARCHAR(100),
 
-);
 
 CREATE TABLE payments(
-Customer_id     INT,
-payment_id      INT,
-payment_date    datetime,
-amount          decimal,
+customerId     INT REFERENCES Customers(customerId),
+paymentId      SERIAL primary key,
+paymentDate    date,
+amount          decimal
 
 );
 
 CREATE TABLE products(
-product_id     INT,
-product_name   VARCHAR(50),
-descriptions   VARCHAR(50),
-buy_price      decimal,
+productId     SERIAL primary key,
+productName   VARCHAR (50),
+descriptions   VARCHAR(200),
+buyPrice      decimal
+);
+
+CREATE TABLE orders(
+orderId           SERIAL primary key,
+productId         INT  REFERENCES products(productId),
+paymentId         INT  REFERENCES payments(paymentId),
+employeeId        INT   REFERENCES employees(employeeId),
+dateRequired      date,
+dateShipped       date,
+parcelStatus      VARCHAR(100)
 
 );
 
 
-
-
--- #5 insert the data to the tables
-INSERT INTO Customer(Customer_ID, First_Name, Last_Name, Gender, Address, Phone, Email, City, Country ) VALUES
-    (1	'Charl'	'Mullar'	'MALE'	'2 WERNIRRING'	23456789	'charl@gmail.com'	'berlin'	'Germany');
-​
+--#5 insert the data to the tables
+--INSERTING INTO TABLES
+INSERT INTO customers(customerid,firstname,lastname,gender,addresses,phone,email,city,country)
+                  VALUES(1, 'John','Hibert','Male','284 chaucer st',084789657,'john@gmail.com','Johannesburg','South'),
+                        (2, 'Thando','Sithole','Female','240 Sect 1',0794445584,'thando@gmail.com','Cape Town','South Africa'),
+                        (3, 'Leon','Glen','Male','81 Everton Rd,Gillits',0820832830,'Leon@gmail.com','Durban','South Africa'),
+                        (4, 'Charl','Muller','Mal','290A Dorset Ecke',+44856872553,'Charl.muller@yahoo.com','Berlin','Germany'),
+                        (5, 'Julia','Stein','Female','2 Wernerring',+448672445058,'Js234@yahoo.com','Frankfurt','Germany');
+INSERT INTO employees(employeeId,firstName,lastname,email,jobtitle)
+            VALUES(1,'Kani','Matthew','mat@gmail.com','Manager'),
+                  (2,'Lesly','Cronje','LesC@gmail.com','Clerk'),
+                  (3,'Gideon','Maduku','m@gmail.com','Accountant');
+INSERT INTO payments(customerId,paymentDate,amount)
+            VALUES(1,'01-09-2018',150.75),
+                  (5,'03-09-2018',150.75),
+                  (4,'03-09-2018',700.60);
+INSERT INTO PRODUCTS(productName,descriptions,buyPrice)
+            VALUES('Harley Davidson Chopper','This replica features working kickstand, front suspension, gear-shift lever',150.75),
+                  ('Classic Car','Turnable front wheels, steering function',550.75),
+                  ('Sports car','Turnable front wheels, steering function',700.60);
+INSERT INTO Orders(productId,paymentId,employeeId,dateRequired,dateShipped,parcelStatus)
+            VALUES(1,1,2,'05-09-2018',NULL,'Not shipped'),
+                  (1,2,2,'04-09-2018','03-09-2018','Shipped'),
+                  (3,3,3,'06-09-2018',NULL,'Not shipped');
+      
